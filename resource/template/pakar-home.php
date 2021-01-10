e02d961e75bb9ce46efb367408c750e24864c5e3
Isal Cahya <cahyaisal13@gmail.com>
2021-01-10 11:52:08 +0700

    update home background

--- resource/template/pakar-home.php
+++ resource/template/pakar-home.php
@@ -1,7 +1,7 @@
 <?php
 include_once( 'pakar-header.php' );
 ?>
-<img src="<?= get_dist_directory().'/assets/img/sistem-pakar.png' ?>" alt="">
+<img src="<?= get_dist_directory().'/assets/img/sistem-pakar-2.png' ?>" alt="">
 	<!-- Hero -->
 <!-- <section class="section-header pb-9 pb-lg-12 text-white">
     <div class="container">
